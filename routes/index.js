804965c2bd346a3e37237324c34cad15aa4d95a9
matrouk <hmm5522@hotmail.com>
2023-11-24 14:22:04 +0300

    Needs more work but database implemented + you can login using google ! will work on it more

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req,res) =>{
+    res.render('main',{
+layout: 'main'
+    })
+    
+})
+
+
+router.get('/login', (req,res) =>{
+    res.render('login',{
+        layout: 'login'
+            })
+    
+})
+
+router.get('/dashboard', (req,res) =>{
+    res.render('dashboard',
+    {layout :'main'})
+    
+})
+
+router.get('/CreatePC', (req,res) =>{
+    res.render('createPc',
+    {layout :'main'})
+    
+})
+
+module.exports = router
