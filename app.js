a28c53c42fd7dca0ad8f56b5fe2b6a162d119fcc
Feras532 <saf_523_@hotmail.com>
2023-11-28 21:10:07 +0300

    clear noise

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
-const path = require('path')
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
-const morgan = require('morgan')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo');
-const exphbs = require('express-handlebars')
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const exphbs = require("express-handlebars");
 
 // Load config
-dotenv.config({ path: 'config/config.env' })
+dotenv.config({ path: "config/config.env" });
 
-connectDB()
-
+connectDB();
 
 // Passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // static folder
-app.use(express.static((path.join(__dirname,'public'))))
-
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //session
 app.use(
   session({
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 
-
 //passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
-app.use('/',require('./routes/index'))
-app.use('/auth', require('./routes/auth'))
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 //handlebars
-app.engine('.hbs', exphbs.engine({defaultLayout: 'main',extname: '.hbs'}))
-app.set('view engine','.hbs')
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+);
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,24 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req,res) =>{
-    res.render('main',{
-layout: 'main'
-    })
-    
-})
+router.get("/", (req, res) => {
+  res.render("main", {
+    layout: "main",
+  });
+});
 
+router.get("/login", (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+});
 
-router.get('/login', (req,res) =>{
-    res.render('login',{
-        layout: 'login'
-            })
-    
-})
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", { layout: "main" });
+});
 
-router.get('/dashboard', (req,res) =>{
-    res.render('dashboard',
-    {layout :'main'})
-    
-})
+router.get("/CreatePC", (req, res) => {
+  res.render("createPc", { layout: "main" });
+});
 
-router.get('/CreatePC', (req,res) =>{
-    res.render('createPc',
-    {layout :'main'})
-    
-})
-
-module.exports = router
+module.exports = router;
