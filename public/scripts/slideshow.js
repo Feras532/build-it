18176499f3738c73ddff6be2990baaad56d859e1
Feras532 <saf_523_@hotmail.com>
2023-12-08 21:54:48 +0300

    more ui changes

--- public/scripts/slideshow.js
+++ public/scripts/slideshow.js
@@ -1,14 +1,25 @@
-    //HOMEPAGE SLIDESHOW JS//
-    let slideIndex = 0;
-    showSlides();
-    
-    function showSlides() {
-      let slides = document.getElementsByClassName("slides");
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex === slides.length) {slideIndex = 0}    
-      slides[slideIndex].style.display = "block";  
-      setTimeout(showSlides, 8000); // Change image every 2 seconds
-    }
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 5000);
+}
