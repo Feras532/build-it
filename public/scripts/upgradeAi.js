608efba21e7003fbb0b572d39c3f3e60a39e3b47
Khaled T <khalidtantawi8@gmail.com>
2023-12-12 23:02:11 +0300

    UPGRADE DONE WOOO

--- public/scripts/upgradeAi.js
+++ public/scripts/upgradeAi.js
@@ -60,7 +60,7 @@ async function fetchData() {
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${OPENAI_KEY}`,
+      Authorization: `Bearer ${API_KEY}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
